4c8a45434c292546a5d934c78f08503ca1e8adbe
OhHanSae <han01240124@gmail.com>
2025-04-18 10:45:06 +0900

    버튼 상호작용

--- index.js
+++ index.js
@@ -3,10 +3,10 @@
 // 페이징 구현
 
 import { ActionRowBuilder, ButtonBuilder, ButtonStyle, Client, EmbedBuilder, Events, GatewayIntentBits, StringSelectMenuBuilder, StringSelectMenuOptionBuilder } from 'discord.js';
-import dotenv from 'dotenv';
-import path from 'path'
-import { fileURLToPath } from 'url';
 import Database from 'better-sqlite3';
+import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 dotenv.config({ path: 'env/token.env' });
 const db = new Database(path.join(path.dirname(fileURLToPath(import.meta.url)), 'db', 'egoGift.db'));
@@ -27,35 +27,59 @@ client.on(Events.InteractionCreate, async interaction => {
         const keyword = interaction.options.getString('키워드');
         const name = interaction.options.getString('이름');
 
-        let query = `SELECT * FROM gift WHERE 1 = 1`;
+        // 기본 쿼리문과 파라미터 선언
+        let query = `SELECT g.name, k.name as 'keyword', g.tire, g.comb, g.hard, g.limited FROM gift g JOIN keyword k ON g.keyword = k.id WHERE 1 = 1`;
         let queryParams = [];
 
+        // where문 쿼리추가, 파라미터 추가
         if (tire) {
-            query += ` AND tire = ?`;
+            query += ` AND g.tire = ?`;
             queryParams.push(tire);
         }
         if (keyword) {
-            query += ` AND keyword = ?`;
+            query += ` AND g.keyword = ?`;
             queryParams.push(keyword);
         }
         if (name) {
-            query += ` AND name LIKE ?`;
+            query += ` AND g.name LIKE ?`;
             queryParams.push(`%${name}%`);
         }
+        query += ` ORDER BY g.tire, g.keyword`
 
         const giftResult = db.prepare(query).all(...queryParams);
-
-        const embed = embedBuilder();
-        const buttons = pageButtonBuilder(false);
-
         console.log(giftResult);
 
-        await interaction.editReply({
-            embeds: [embed],
-            components: [buttons]
+        // 결과값 3개씩 나눠서 2차원배열로 정리
+        let giftList = [];
+        for (let i = 0; i < giftResult.length; i += 3) {
+            const chunk = giftResult.slice(i, i + 3);
+            giftList.push(chunk);
+        }
+
+        console.log(giftList)
+
+        const response = await interaction.editReply({
+            embeds: [embedBuilder()],
+            components: [pageButtonBuilder(false)]
         });
 
-        return;
+        const collector = response.createMessageComponentCollector({
+            time: 180_000
+        });
+
+        collector.on('collect', async i => {
+
+
+            // collector.stop();
+        })
+
+        // collector.on('end', async () => {
+        //     const disabledButtons = pageButtonBuilder(true);
+        //     await interaction.editReply({
+        //         components: [disabledButtons]
+        //     });
+        //     return;
+        // });
     }
 });
 
@@ -73,27 +97,27 @@ function pageButtonBuilder(disable) {
                 .setCustomId('back')
                 .setLabel('⏪이전')
                 .setStyle(ButtonStyle.Primary)
-                .setDisabled(disable ? true : false),
+                .setDisabled(disable),
             new ButtonBuilder()
                 .setCustomId('1')
                 .setLabel('1️⃣')
                 .setStyle(ButtonStyle.Primary)
-                .setDisabled(disable ? true : false),
+                .setDisabled(disable),
             new ButtonBuilder()
                 .setCustomId('2')
                 .setLabel('2️⃣')
                 .setStyle(ButtonStyle.Primary)
-                .setDisabled(disable ? true : false),
+                .setDisabled(disable),
             new ButtonBuilder()
                 .setCustomId('3')
                 .setLabel('3️⃣')
                 .setStyle(ButtonStyle.Primary)
-                .setDisabled(disable ? true : false),
+                .setDisabled(disable),
             new ButtonBuilder()
                 .setCustomId('next')
                 .setLabel('다음⏩')
                 .setStyle(ButtonStyle.Primary)
-                .setDisabled(disable ? true : false),
+                .setDisabled(disable),
         )
 }
 
